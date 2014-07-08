2814b32cfcb692f89825968041b4b04423194c03
Empower Pune <empower.puneteam@empower-solutions.biz>
2014-07-08 19:14:16 +0530

    Push the php file

--- admin/hello.php
+++ admin/hello.php
@@ -1,5 +1,12 @@
 <?php 
 
-echo 'push admin folder';
-echi 'push line 2';
+echo  '<h1 style="color:red;"> LDS Test Page </h1>';
+echo '<br><br><br>';
+echo  '<h3>Here you see the changes as push on git:</h3>';
+echo '<br><br>';
+echo 'push: admin folder!';
+echo '<br>';
+echo 'push: new data added!';
+echo '<br>';
+
 ?>
