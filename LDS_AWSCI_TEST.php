551883bd5d1c339f837aff442f731285b6ad25c9
Empower Pune <empower.puneteam@empower-solutions.biz>
2014-07-10 11:18:30 +0530

    Push Test

--- LDS_AWSCI_TEST.php
+++ LDS_AWSCI_TEST.php
@@ -1,6 +1,6 @@
 <?php 
 
-echo  '<h1 style="color:red;"> LDS Test Page 10 July 11:08 AM </h1>';
+echo  '<h1 style="color:red;"> LDS Test Page 10 July 11:17 AM </h1>';
 echo '<br><br><br>';
 echo  '<h3>Here you see the changes as pushed on git:</h3>';
 echo '<br><br>';
